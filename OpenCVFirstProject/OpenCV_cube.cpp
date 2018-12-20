fd180062259d6ce5a0698e522bc83c0cb5d2dd20
krzyyyy <32521084+krzyyyy@users.noreply.github.com>
2018-12-20 18:16:01 +0100

    Add files via upload

--- OpenCVFirstProject/OpenCV_cube.cpp
+++ OpenCVFirstProject/OpenCV_cube.cpp
@@ -34,7 +34,8 @@ void mul(const Mat& src, vector<Vec3f> pts, vector<Vec3f>& out );
 void mean(const vector<Vec3f>& points, vector<vector<int>>& id_img, vector<Vec3f>& out );
 //void find_visiable()
 int main() {
-	Mat img = imread("marcin.png");
+	Mat img1 = imread("marcin.png");
+	vector <Vec2f> corner_points({Vec2f(0,0),Vec2f(img1.cols,0), Vec2f(0,img1.rows),Vec2f(img1.cols,img1.rows)} );
 	for(auto& x : band)
 		for(auto y:band)
 			for(auto z:band)
@@ -74,7 +75,7 @@ int main() {
 			drawMarker(img, Point(p+Vec2f(200,200)), Scalar(200,0,120));
 		for(auto p:points_mean_2d)
 			drawMarker(img, Point(p+Vec2f(200,200)), Scalar(50,0,220));
-
+		vector <size_t> indexes_good(0);
 		vector <size_t> indexes_of_id({0, 1, 2, 3, 4, 5});//iota nie dziala na tym kompilatorze:)
 		sort(indexes_of_id.begin(), indexes_of_id.end(),
 				[&points_mean_3d](size_t a, size_t b){return points_mean_3d[a][2]>points_mean_3d[b][2];});
@@ -84,20 +85,28 @@ int main() {
 			for(auto pts:id_img_points_good){
 				vector <Vec2f> area(4);
 				vector <Vec2f> area2(0);
-				generate(area.begin(), area.end(),[points_2d, pts, n=0]()mutable{return points_2d[pts[n++]]+Vec2f(201,201);});
+				generate(area.begin(), area.end(),[points_2d, pts, n=0]()mutable{return points_2d[pts[n++]];});
 				convexHull(area, area2);
-				if(pointPolygonTest(area2, points_mean_2d[id]+Vec2f(201,201), false)>=0)
+				if(pointPolygonTest(area2, points_mean_2d[id], false)>=0)
 					if_internal=true;
 			}
-			if(if_internal==false)
+			if(if_internal==false){
 				id_img_points_good.push_back(id_img_points[id]);
+				indexes_good.push_back(id);
+			}
 		}
 
 		for(auto p:id_img_points_good)
 			for(auto pt:p)
 				drawMarker(img, Point(points_2d[pt]+Vec2f(200,200)), Scalar(100, 200,10));
 
-
+		for(auto id:indexes_good){
+			vector <Vec2f> wall_points(4);
+			generate(wall_points.begin(), wall_points.end(),
+						[id, id_img_points, points_2d, n=0]()mutable{return points_2d[id_img_points[id][n++]]+Vec2f(200,200);});
+			Mat mat_transf = getPerspectiveTransform(corner_points, wall_points);
+			warpPerspective(img1, img, mat_transf, Size(img1.rows, img1.cols));
+		}
 
 		imshow("okno", img);
 		sign = waitKey(0);
